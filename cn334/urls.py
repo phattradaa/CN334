8b010db85e08609a678addb7a409209d4889a4aa
phattradaa <phattrada.mik@dome.tu.ac.th>
2024-04-01 01:02:54 +0700

    W09

--- cn334/urls.py
+++ cn334/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path("ecommerce/", ecom_views.ecommerce_index_view),
     path("ecommerce/item/<item_id>",ecom_views.item_view),
     path("w09/request",ecom_views.basic_request),
-    path("w09/tokenize",ecom_views.tokenize)
+    path("w09/tokenize",ecom_views.tokenize),
+    path("w09/sentimental",ecom_views.sentimental),
+    path("w09/text2speech",ecom_views.text2speech)
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+import json
 
 # Create your views here.
 
@@ -44,3 +45,42 @@ def tokenize(request):
         return JsonResponse({"student":"student_id","tokenize":response_json},safe=False)
     
     return JsonResponse({"error":"Method not allowed"}, safe=False, status=403)
+
+@csrf_exempt
+def sentimental(request):
+    if request.method == "POST":
+        try:
+            sentence = request.POST['text']
+        except:
+            return JsonResponse({"error":"Input not found"},safe=False, status=500)
+        url = "https://api.aiforthai.in.th/ssense"
+        data = {'text':sentence}
+        headers = {
+            'Apikey' :"7Nnz7YqING7Q3LMriW4O4ph8VTK6I6aH"
+        }     
+        response = requests.post(url, data=data, headers=headers)
+        response_json = response.json()
+        return JsonResponse({"student":"6410742099","sentiment":response_json},safe=False)
+    
+    return JsonResponse({"error":"Method not allowed"}, safe=False, status=403)
+           
+@csrf_exempt
+def text2speech(request):
+    if request.method == "POST":
+        try:
+            text = request.POST['text']
+        except:
+            return JsonResponse({"error":"Input not found"},safe=False, status=500)
+        url = "https://api.aiforthai.in.th/vaja9/synth_audiovisual"
+        data = {'input_text':text,'speaker': 1, 'phrase_break':0, 'audiovisual':0}
+        headers = {
+            'Apikey' :"7Nnz7YqING7Q3LMriW4O4ph8VTK6I6aH"
+        }     
+        response = requests.post(url, json=data, headers=headers)
+        response_json = response.json()
+        print(response_json)
+        #response_dict = json.loads(response)
+        return JsonResponse({"student":"6410742099","output":response_json['wav_url']},safe=False)
+    
+    return JsonResponse({"error":"Method not allowed"}, safe=False, status=403)
+             
